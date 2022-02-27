d75cf0cc490865f72a94053ea6796ec77038073f
Dinesh Rout <rddinesh779@gmail.com>
2022-02-27 22:23:14 +0530

    refactor: added initial markup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,48 @@ import Input from './components/Input';
 import Preview from './components/Preview';
 
 const App = () => {
-  const [markdown, setMarkdown] = useState('## Type something...');
+  const [markdown, setMarkdown] = useState(`
+  # Welcome to my React Markdown Previewer!
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+  - Some are bulleted.
+  - With different indentation levels.
+  - That look like this.
+  
+  1. And there are numbered lists too.
+  2. Use just 1s if you want!
+  3. And last but not least, let's not forget embedded images: ![sample image](https://www.vhv.rs/dpng/d/590-5903330_reactjs-logo-react-js-transparent-icon-hd-png.png)
+  
+  `);
 
   const handleChange = (e) => {
     setMarkdown(e.target.value);
